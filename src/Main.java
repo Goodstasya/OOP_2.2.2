ccdb169225be50a92b45a793f3dc070f876bb1eb
Ilina Anastasiia <goodstasya@gmail.com>
2020-07-09 17:06:20 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float massa = 96;
-        float height = 1.83F;
+        float massa = 68;
+        float height = 1.70F;
         float index = service.calculate(massa, height);
         System.out.println(index);
     }
